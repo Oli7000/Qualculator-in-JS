5c37fa787ee9244ae709cc45e11ab3bcbe6e07cb
Olivier <olivier.luethy@gmx.net>
2021-10-18 16:14:07 +0200

    First Solution - Not good

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-// var eins = 1;
-// var zwei = 2;
-// var drei = 3;
-// var vier = 4;
-// var fuenf = 5;
-// var sex = 6;
-// var sieben = 7;
-// var acht = 8;
-// var neun = 9;
-// var nuell = 0;
-var input = document.getElementById("output");
-
-var total = 0;
-
-function seven() {
-    if (input.value == 0) {
-        document.getElementById("output").value = "";
-        document.getElementById("output").value += 7;
-    } else if (input.value > 0) {
-        document.getElementById("output").value += 7;
-    }
+//function that display value
+function dis(val) {
+    document.getElementById("output").value += val
 }
 
-function clearInput() {
-    document.getElementById("output").value = 0;
+//function that evaluates the digit and return result
+function solve() {
+    let x = document.getElementById("output").value
+    let y = eval(x)
+    document.getElementById("output").value = y
+}
+
+//function that clear the display
+function clr() {
+    document.getElementById("output").value = ""
 }
