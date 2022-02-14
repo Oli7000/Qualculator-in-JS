9018b517c00b6bad2c50334c95c2fe154e24691f
Oli7000 <olivier.luethy@gmx.net>
2022-02-14 15:42:05 +0100

    Calculator works perfectly but not delete one num

--- script.js
+++ script.js
@@ -2,27 +2,46 @@ let numberInInput = 0;
 let secondNumber = 0;
 let operation = "";
 let total = 0;
+let buttonCount = 0;
 
+/* Für die Anzeige zuständig */
 function display(number) {
-    document.getElementById("input").value = number;
-
-    if (numberInInput) {
+    if (buttonCount == 0 && operation == "" && numberInInput == 0 && secondNumber == 0 && total == 0) {
+        document.getElementById("input").value = number;
+        numberInInput = document.getElementById("input").value;
+        buttonCount++;
+        console.log("1", numberInInput)
+    } else if (buttonCount != 0 && operation == "" && numberInInput != 0 && secondNumber == 0 && total == 0) {
+        document.getElementById("input").value += number;
+        numberInInput += number;
+        console.log("2", numberInInput, "", secondNumber)
+    } else if (buttonCount != 0 && operation != "" && secondNumber == 0 && total == 0) {
+        document.getElementById("input").value = number;
+        secondNumber = document.getElementById("input").value;
+        console.log("3", numberInInput, " ", secondNumber)
+    } else if (buttonCount != 0 && operation != "" && secondNumber != 0 && total == 0) {
+        document.getElementById("input").value += number;
+        secondNumber += number;
+        console.log("4", numberInInput, " ", secondNumber);
+    } else if (total != 0) {
+        document.getElementById("input").value = "";
+        document.getElementById("input").value = number;
         secondNumber = number;
-        console.log(secondNumber);
-    } else {
-        numberInInput = number;
-        console.log(numberInInput);
+        console.log("5", secondNumber);
     }
 }
 
+/* Um den Inhalt vom Input Feld zu löschen */
 function clearNum() {
     numberInInput = 0;
     secondNumber = 0;
     total = 0;
-    operation = 0;
+    operation = "";
+    buttonCount = 0;
     document.getElementById("input").value = "0";
 }
 
+/* Falls plus, minus, geteilt, mahl oder modulo ausgewählt wird */
 function display_add(add) {
     document.getElementById("input").value = "";
     document.getElementById("input").value = add;
@@ -53,55 +72,64 @@ function display_modulo(modulo) {
     operation = "modulo";
 }
 
+/* Um das Resultat zu berechnen */
 function solve() {
     if (operation == "addition" && total == 0) {
+        numberInInput = parseInt(numberInInput);
+        secondNumber = parseInt(secondNumber);
+        console.log(numberInInput, secondNumber);
         total = numberInInput + secondNumber;
+        document.getElementById("input").value = "";
+        document.getElementById("input").value = total;
+    } else if (operation == "addition" && total != 0) {
+        secondNumber = parseInt(secondNumber);
+        total += secondNumber;
         document.getElementById("input").value = "";
         document.getElementById("input").value = total;
     } else if (operation == "subtraction" && total == 0) {
         total = numberInInput - secondNumber;
         document.getElementById("input").value = "";
         document.getElementById("input").value = total;
+    } else if (operation == "subtraction" && total != 0) {
+        secondNumber = parseInt(secondNumber);
+        total -= secondNumber;
+        document.getElementById("input").value = "";
+        document.getElementById("input").value = total;
     } else if (operation == "divide" && total == 0) {
         total = numberInInput / secondNumber;
+        document.getElementById("input").value = "";
+        document.getElementById("input").value = total;
+    } else if (operation == "divide" && total != 0) {
+        secondNumber = parseInt(secondNumber);
+        total /= secondNumber;
         document.getElementById("input").value = "";
         document.getElementById("input").value = total;
     } else if (operation == "multiply" && total == 0) {
         total = numberInInput * secondNumber;
         document.getElementById("input").value = "";
         document.getElementById("input").value = total;
+    } else if (operation == "multiply" && total != 0) {
+        secondNumber = parseInt(secondNumber);
+        total *= secondNumber;
+        document.getElementById("input").value = "";
+        document.getElementById("input").value = total;
     } else if (operation == "modulo" && total == 0) {
         total = numberInInput % secondNumber;
         document.getElementById("input").value = "";
         document.getElementById("input").value = total;
-    } else if (operation == "addition" && total != 0) {
-        total = total + secondNumber;
-        document.getElementById("input").value = "";
-        document.getElementById("input").value = total;
-    } else if (operation == "subtraction" && total != 0) {
-        total = total - secondNumber;
-        document.getElementById("input").value = "";
-        document.getElementById("input").value = total;
-    } else if (operation == "divide" && total != 0) {
-        total = total / secondNumber;
-        document.getElementById("input").value = "";
-        document.getElementById("input").value = total;
-    } else if (operation == "multiply" && total != 0) {
-        total = total * secondNumber;
-        document.getElementById("input").value = "";
-        document.getElementById("input").value = total;
     } else if (operation == "modulo" && total != 0) {
-        total = total % secondNumber;
+        secondNumber = parseInt(secondNumber);
+        total %= secondNumber;
         document.getElementById("input").value = "";
         document.getElementById("input").value = total;
     }
 }
 
+/* Um eine ziffer zu löschen */
 function clearLastNumber() {
     let text = document.getElementById("input").value;
     // https://stackoverflow.com/questions/952924/how-do-i-chop-slice-trim-off-last-character-in-string-using-javascript
     text = parseInt(text.substring(0, text.length - 1));
     document.getElementById("input").value = text;
-    numberInInput = text;
-    console.log(numberInInput);
+    secondNumber = text;
 }
